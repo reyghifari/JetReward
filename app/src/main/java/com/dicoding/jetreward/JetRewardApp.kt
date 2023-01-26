65393f1e8c90cddea2550b954194ffe9fce11a82
Kgs Muhammad Raihan Ghifari <raihan.ghifari1603@gmail.com>
2023-01-26 12:06:54 +0700

    Navigation Component Bottom Navigation

--- app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
+++ app/src/main/java/com/dicoding/jetreward/JetRewardApp.kt
@@ -1,15 +1,57 @@
 package com.dicoding.jetreward
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavGraph.Companion.findStartDestination
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.dicoding.jetreward.ui.navigation.NavigationItem
+import com.dicoding.jetreward.ui.navigation.Screen
+import com.dicoding.jetreward.ui.screen.cart.CartScreen
+import com.dicoding.jetreward.ui.screen.home.HomeScreen
+import com.dicoding.jetreward.ui.screen.profile.ProfileScreen
 import com.dicoding.jetreward.ui.theme.JetRewardTheme
 
 @Composable
 fun JetRewardApp(
     modifier: Modifier = Modifier,
+    navController: NavController = rememberNavController()
 ) {
-
+    Scaffold(
+        bottomBar = {
+            BottomBar(navController)
+        },
+        modifier = modifier
+    ){ 
+       NavHost(
+           navController = navController as NavHostController,
+           startDestination = Screen.Home.route,
+           modifier = Modifier.padding(it)
+       ){
+            composable(Screen.Home.route){
+                HomeScreen()
+            }
+           composable(Screen.Cart.route) {
+                CartScreen()
+            }
+           composable(Screen.Profile.route) {
+               ProfileScreen()
+           }
+       }
+    }
 }
 
 @Preview(showBackground = true)
@@ -17,5 +59,55 @@ fun JetRewardApp(
 fun JetHeroesAppPreview() {
     JetRewardTheme {
         JetRewardApp()
+    }
+}
+
+@Composable
+fun BottomBar(
+    navController: NavController,
+    modifier: Modifier = Modifier
+) {
+    BottomNavigation(
+        modifier = modifier
+    ) {
+        val navBackStackEntry by navController.currentBackStackEntryAsState()
+        val currentRoute = navBackStackEntry?.destination?.route
+
+        val navigationItem = listOf(
+            NavigationItem(
+                title = stringResource(R.string.menu_home),
+                icon = Icons.Default.Home,
+                screen = Screen.Home
+            ),
+            NavigationItem(
+                title = stringResource(R.string.menu_cart),
+                icon = Icons.Default.ShoppingCart,
+                screen = Screen.Cart
+            ),
+            NavigationItem(
+                title = stringResource(R.string.menu_profile),
+                icon = Icons.Default.AccountCircle,
+                screen = Screen.Profile
+            )
+        )
+        BottomNavigation {
+            navigationItem.map {
+                navigationItem ->
+                BottomNavigationItem(
+                    selected = currentRoute == navigationItem.screen.route,
+                    onClick = { navController.navigate(navigationItem.screen.route){
+                        popUpTo(navController.graph.findStartDestination().id){
+                            saveState = true
+                        }
+                        restoreState = true
+                        launchSingleTop = true
+                    } },
+                    label = { Text(text = navigationItem.title)},
+                    icon = { Icon(
+                        imageVector = navigationItem.icon,
+                        contentDescription = navigationItem.title)}
+                )
+            }
+        }
     }
 }
