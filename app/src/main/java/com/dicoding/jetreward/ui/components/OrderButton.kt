96d0d419211e950f5f05292267d322f411e3bd85
Kgs Muhammad Raihan Ghifari <raihan.ghifari1603@gmail.com>
2023-01-27 20:18:15 +0700

    JetReward Testing

--- app/src/main/java/com/dicoding/jetreward/ui/components/OrderButton.kt
+++ app/src/main/java/com/dicoding/jetreward/ui/components/OrderButton.kt
@@ -7,6 +7,7 @@ import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.dicoding.jetreward.ui.theme.JetRewardTheme
@@ -21,11 +22,11 @@ fun OrderButton(
     Button(
         onClick = onClick,
         enabled = enabled,
-        modifier = modifier.fillMaxWidth().height(52.dp)
+        modifier = modifier.fillMaxWidth().height(52.dp),
     ) {
         Text(
             text = text,
-            modifier = Modifier.align(Alignment.CenterVertically)
+            modifier = Modifier.align(Alignment.CenterVertically),
         )
     }
 }
